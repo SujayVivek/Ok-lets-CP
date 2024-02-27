936ba4ed8f1b6822765b6db8cf97cd731b476cf2
SujayVivek <sujayv2016@gmail.com>
2024-02-27 15:08:42 +0530

    Codeforces 800 Make Equal- Just normal logic

---
+++ 800-1000/B_Make_Equal.cpp
@@ -0,0 +1,40 @@
+
+//   _______ _     _                     _        _        __             _   _            _                            
+//  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
+//     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
+//     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
+//     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
+//     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
+                                                                                                                     
+#include <bits/stdc++.h>
+using namespace std;                                                                                                                     
+#define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
+typedef vector<int> vi;
+#define PB push_back
+
+void Solve(){
+    int n;cin>>n;
+    vi v(n,0);int sum=0;
+    FOR(i,n){
+       cin>>v[i]; 
+       sum+=v[i];
+    }
+    sum=sum/n;
+    int ctr=0;
+    FOR(i,n){
+        ctr+=v[i]-sum;
+        if(ctr<0){
+            cout<<"NO"<<endl;return;
+        }
+    }
+    cout<<"YES"<<endl;return;
+
+
+}
+int main(){
+    int t;cin>>t;
+    while(t--){
+        Solve();
+    }
+    return 0;
+}
