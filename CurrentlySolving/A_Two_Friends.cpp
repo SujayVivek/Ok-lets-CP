a071b3f40bab2acda562f22725d2d7e2a65179d8
SujayVivek <sujayv2016@gmail.com>
2024-05-01 20:03:59 +0530

    Two friends div2 round 941

--- CurrentlySolving/A_Two_Friends.cpp
+++ CurrentlySolving/A_Two_Friends.cpp
@@ -8,26 +8,31 @@
                                                                                                                      
 #include <bits/stdc++.h>
 using namespace std;                                                                                                                     
-#define FOR(a,c)   for ( long long (a)=1; (a)<(c); (a)++)
-typedef vector<long long> vi;
-// typedef vector<long long> vll;
+#define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
+typedef vector<int> vi;
+typedef vector<long long> vll;
 #define PB push_back
 #define ll long long
 
 int main(){
     int t;cin>>t;
     while(t--){
-        long long n;cin>>n;
-        vi p(n+1,0);
-        p[0]=0;
-        FOR(i,n+1){
-            cin>>p[i];
-            // p[i]=p[i]-1;
-        }
-        long long ctr=0;
-        FOR(i,n+1){
-            if(p[i]==p[p[i]]){
-                ctr=1;break;
+        int n;cin>>n;
+        vi v(n+1,0);
+
+       for(int i=1;i<=n;i++){
+        cin>>v[i];
+       }
+        int ctr=0;
+        v[0]=0;
+    //     for(int i=0;i<=n;i++){
+    //     cout<<v[i]<<" ";
+    //    }
+        for(int i=1;i<=n;i++){
+            for(int j=i+1;j<=n;j++){
+                if(v[i]==j && i==v[j]){
+                    ctr=1;
+                }
             }
         }
         if(ctr==1){
