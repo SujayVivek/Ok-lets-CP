26d0f7de71004b5582f1e6125ef6b1858c025166
SujayVivek <sujayv2016@gmail.com>
2024-05-14 15:58:39 +0530

    Crazy qn- combinatorics 1300 TLE 1

--- D_Divisible_Pairs.cpp
+++ D_Divisible_Pairs.cpp
@@ -14,61 +14,23 @@ typedef vector<long long> vll;
 #define PB push_back
 #define ll long long
 
-int Checker(int a,int b,int x, int y){
-    if((a+b)%x == 0 && (abs(a-b))%y ==0){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
+
 
 int main(){
     int t;cin>>t;
-    while(t--){int ctr=0;
-        int n,x,y;cin>>n>>x>>y;
-        vi ve, vo;
+    while(t--){
+        ll n,x,y;cin>>n>>x>>y;
+        // vi v(n,0);
+        map<pair<ll, ll>,ll>mp;
+        long long ans = 0;
         FOR(i,n){
-            int l;
-            cin>>l;
-            if(l%2==0)ve.push_back(l);
-            else vo.push_back(l);
+            long long a;cin>>a;
+            ans+= mp[{(x-a%x)%x, a%y}];
+            pair <ll, ll> p = {a%x, a%y};
+            mp[p]++;
         }
-        sort(ve.begin(), ve.end());
-        sort(ve.begin(), ve.end());
-        if(x%2==0){
-            //use only the even array
-            // int ctr=0;
-            int sz= ve.size();
-            // int j=1,i=0;
-            for(int i=0;i<n-1;i++){
-                for(int j=i+1;j<n;j++){
-                    if(ve[i]+ ve[j]< x || abs(ve[i] - ve[j])<y){
-                        continue;
-                    }
-                    if(Checker(ve[i], ve[j], x, y)){
-                        ctr++;
-                    }
-                }
-            }
-        }
-        else if(x%2!= 0){
-            int p= ve.size(), q= vo.size();
-            int i=0,j=0;
-            for(int i=0;i<p;i++){
-                for(int j=0;j<q;j++){
-                    if(ve[i]+ vo[j]< x || abs(ve[i] - vo[j])<y){
-                        continue;
-                    }
-                    if(Checker(ve[i], vo[j], x, y)){
-                        ctr++;
-                    }
-                }
-            }  
-        }
-        cout<<ctr<<endl;
+        cout<<ans<<endl;
 
     }
-    
     return 0;
 }
