64b5f0ceff11b68eda8b43a12e18fd532b44a06f
SujayVivek <sujayv2016@gmail.com>
2024-05-17 23:37:53 +0530

    created snippet2

--- Snippet2.cpp
+++ Snippet2.cpp
@@ -1,9 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define MOD 1000000007
 typedef long long ll;
-#define loop(ii,i,n) for(ll ii = i; ii < n; ++ ii)
+#define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
 #define INF 1e18
 #define endl "\n"
 #define pb push_back
@@ -17,24 +21,30 @@ typedef unsigned long long ull;
 typedef long double lld;
 typedef vector<int> vi;
 typedef vector<pair<ll, ll>> vpii;
-typedef vector<vi>  vvi;
- 
-void Solve(){
+typedef vector<vi> vvi;
 
+void Solve() {
+    // Your code for each test case goes here
 }
 
- 
-int main() {
-	fastio();
-#ifndef ONLINE_JUDGE
-	freopen("error.txt", "w", stderr);
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-	int tt_ = 1;
-	cin >> tt_;
-	while (tt_--) {
-		Solve();
-	}
-	return 0;
+int32_t main() {
+    auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
+
+    fastio();
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
 }
