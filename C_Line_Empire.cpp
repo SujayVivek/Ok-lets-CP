8591d9822d50b8baf09a09ac44014ecc608d36bd
SujayVivek <sujayv2016@gmail.com>
2024-05-24 00:00:12 +0530

    INgenuiy

--- C_Line_Empire.cpp
+++ C_Line_Empire.cpp
@@ -35,116 +35,15 @@ typedef vector<long long> vll;
 typedef vector<vll> vvll;
 
 void Solve() {
-
-    int n;
-    string s;
-    cin >> n >> s;
- 
-    int N = count(all(s), 'N');
-    int S = count(all(s), 'S');
-    int E = count(all(s), 'E');
-    int W = count(all(s), 'W');
-    bool ok = (N % 2 == S % 2);
-    ok &= (E % 2 == W % 2);
- 
-    if (!ok)
-    {
-        cout<<"NO"<<endl;
-        return;
+    // Your code for each test case goes here
+    int n,a,b;
+    cin>>n>>a>>b;
+    vi v(n,0);
+    loop(i,0,n){
+        cin>>v[i];
     }
     
-    map<char, vector<int>> mp;
-    for (int i = 0; i < n; ++i)
-        mp[s[i]].push_back(i);
-    
-    string ans = s; 
-    vector<int>& ntmp = mp['N'], stmp = mp['S'];
- 
-    int j = 0;
-    while (j < min(sz(ntmp), sz(stmp)))
-    {
-        if (j % 2)
-            ans[ntmp[j]] = ans[stmp[j]] = 'R';
-        else
-            ans[ntmp[j]] = ans[stmp[j]] = 'H';
-        ++j;
-    }
- 
-    while (j < sz(ntmp))
-    {
-        if (j % 2)
-            ans[ntmp[j]] = 'R';
-        else
-            ans[ntmp[j]] = 'H';
-        ++j;
-    }
- 
-    while (j < sz(stmp))
-    {
-        if (j % 2)
-            ans[stmp[j]] = 'R';
-        else
-            ans[stmp[j]] = 'H';
-        ++j;
-    }
- 
-    vector<int>& etmp = mp['E'], wtmp = mp['W'];
-    j = 0;
-    while (j < min(sz(etmp), sz(wtmp)))
-    {
-        if (j % 2)
-            ans[etmp[j]] = ans[wtmp[j]] = 'R';
-        else
-            ans[etmp[j]] = ans[wtmp[j]] = 'H';
-        ++j;
-    }
- 
-    while (j < sz(etmp))
-    {
-        if (j % 2)
-            ans[etmp[j]] = 'R';
-        else
-            ans[etmp[j]] = 'H';
-        ++j;
-    }
- 
-    while (j < sz(wtmp))
-    {
-        if (j % 2)
-            ans[wtmp[j]] = 'R';
-        else
-            ans[wtmp[j]] = 'H';
-        ++j;
-    }
- 
-    int h = count(all(ans), 'H');
-    int r = count(all(ans), 'R');
- 
-    if (r == 0 || h == 0)
-    {
-        if (N % 2 && E == 0)
-        {
-            cout<<"NO"<<endl;
-            return;
-        }
-        if (E % 2 && N == 0)
-        {
-            cout<<"NO"<<endl;
-            return;
-        }
- 
-        for (int i = 0; i < n; ++i)
-            if (s[i] == 'N' || s[i] == 'S')
-                ans[i] = 'H';
-            else
-                ans[i] = 'R';
-    }
- 
-    cout << ans << "\n";
-
-        return;
 }
-
 
 int32_t main() {
     auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
