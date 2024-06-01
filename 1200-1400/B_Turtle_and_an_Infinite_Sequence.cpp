2f324f5acb57395ff7cf2b5dc05ab0d16208a5a7
SujayVivek <sujayv2016@gmail.com>
2024-06-01 14:30:34 +0530

    lol

---
+++ 1200-1400/B_Turtle_and_an_Infinite_Sequence.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//   _______ _     _                     _        _        __             _   _            _                            
+//  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
+//     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
+//     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
+//     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
+//     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define MOD 1000000007
+typedef long long ll;
+#define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
+#define loopm(ii, i, n, p) for (ll ii = i; ii < n; ii+p)
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+typedef unsigned long long ull;
+typedef long double lld;
+typedef vector<int> vi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vi> vvi;
+
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+
+void Solve() {
+    int t;
+    cin >> t;
+    vector<ll> fact;
+    fact.resize(30);
+    fact[0] = 1;
+    for (int i = 1; i < 30; i++)
+    {
+        fact[i] = fact[i - 1] * 2;
+    }
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        ll ans = 0;
+        ll bigN = n + m;
+        ll smallN = max(1ll*0, n - m);
+        ans = bigN;
+        //cout<<l<<" "<<r<<endl;
+        ll gap = bigN - smallN + 1;
+        if (gap == 1)
+        {
+            cout << n << endl;
+        }
+        
+        else
+        {
+ 
+            for (int i = 0; i < 31; i++)
+            {
+                if (gap >= fact[i]+1)
+                {
+                    ans |= (1 << i);
+                }
+                else
+                {
+                    if(smallN&(1<<i) || bigN&(1<<i))
+                    {
+                        ans|=(1<<i);
+                    }
+                }
+            }
+            cout << ans << endl;
+        }
+    }
+    // return 0;
+}
+
+int32_t main() {
+    auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
+
+    fastio();
+    // #ifndef ONLINE_JUDGE
+    //     freopen("error.txt", "w", stderr);
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    int tt_ = 1;
+    // cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+    // auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
