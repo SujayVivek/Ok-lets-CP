763821c711c840254d86bacc0863f9d37a2167db
SujayVivek <sujayv2016@gmail.com>
2024-06-19 11:15:34 +0530

    .

--- B_Make_It_Ugly.cpp
+++ B_Make_It_Ugly.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 //   _______ _     _                     _        _        __             _   _            _                            
 //  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
 //     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
 //     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
 //     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
 //     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
-
+ 
 mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
-
+ 
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define MOD 1000000007
 typedef long long ll;
 #define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
-#define loopm(ii, i, n, p) for (ll ii = i; ii < n; ii+=p)
+#define loopm(ii, i, n, p) for (ll ii = i; ii < n; ii+p)
 #define INF 1e18
 #define endl "\n"
 #define pb push_back
@@ -30,32 +30,77 @@ typedef long double lld;
 typedef vector<int> vi;
 typedef vector<pair<ll, ll>> vpii;
 typedef vector<vi> vvi;
-
+ 
 typedef vector<long long> vll;
 typedef vector<vll> vvll;
-
-bool comp(const vi &a, const vi &b) {
-    return a[1] > b[1];
-}
-
+ 
 void Solve() {
-    int n, m, k;
-    cin>>n>>m>>k; vll v(n,0);
-    loop(i,0,n)cin>>v[i];
-    sort(all(v));
-    ll b = 0;ll sum = 0;
-    loop(i,0,n){
-        ll mn = min(k, m);
-        sum+= mn*(v[i]+b);
-        k-=mn;
-        b+= mn;
+    int n;
+    cin >> n;
+    list<int> v;
+    set<int> st;
+    loop(i, 0, n) {
+        int l;
+        cin >> l;
+        v.push_back(l);
+        
+        st.insert(l);
     }
-    cout<<sum<<endl;
- }
-
+    if (st.size() == 1) {
+        cout << -1 << endl;
+        return;
+    }
+ 
+    // Copy list v to v2
+    list<int> v2 = v;
+ 
+    int ctr = 0;
+    int mx = 0;
+    auto it = v.begin();
+ 
+    while (it != v.end()) {
+        auto it2 = it;
+        advance(it2, 2);
+ 
+        if (it2 != v.end() && *it == *it2) {
+            ctr++;
+            it2 = v.erase(it2);
+        } else {
+            mx = max(mx, ctr);
+            ctr = 0;
+            if (it != v.end()) it++;
+        }
+    }
+    mx = max(mx, ctr); // Update mx for the last count
+ 
+    int mx2 = 0;
+    ctr = 0;
+    it = v2.end(); // Re-initialize iterator for v2
+ 
+    while (it != v2.begin()) {
+        auto it2 = it;
+        advance(it2, -2);
+ 
+        if (it2 != v.begin() && *it == *it2) {
+            ctr++;
+            it2 = v.erase(it2);
+        } else {
+            mx2 = max(mx2, ctr);
+            ctr = 0;
+            if (it != v.begin()) it--;
+        }
+    }
+    mx2 = max(mx2, ctr); // Update mx2 for the last count
+    mx = min(mx, mx2);
+    cout << mx << endl;
+}
+ 
+ 
+ 
+ 
 int32_t main() {
     auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
-
+ 
     fastio();
     // #ifndef ONLINE_JUDGE
     //     freopen("error.txt", "w", stderr);
@@ -68,7 +113,7 @@ int32_t main() {
     while (tt_--) {
         Solve();
     }
-
+ 
     // auto end = chrono::high_resolution_clock::now();
     // auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
     // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
