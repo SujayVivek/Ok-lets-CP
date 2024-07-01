796ddfc8661f70c981e93ef548f7e52c1d05fa1c
SujayVivek <sujayv2016@gmail.com>
2024-07-01 14:26:19 +0530

    .

---
+++ D_Smithing_Skill.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//   _______ _     _                     _        _        __             _   _            _                            
+//  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
+//     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
+//     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
+//     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
+//     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define MOD 1000000007
+typedef long long ll;
+#define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
+#define loopm(ii, i, n, p) for (ll ii = i; ii < n; ii+p)
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+typedef unsigned long long ull;
+typedef long double lld;
+typedef vector<int> vi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vi> vvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+
+void Solve() {
+    // Your code for each test case goes here
+    ll n,m;cin>>n>>m;
+    vvll v(n,vll(2));
+    vll q(m,0);
+    // memset(v,,sizeof(v));
+    loop(i,0,n){
+        cin>>v[i][1];
+    }
+    loop(i,0,n){
+        cin>>v[i][0];
+    }
+    loop(i,0,m){
+        cin>>q[i];
+    }
+    
+    int ctr=0;
+    // sort(all(v));reverse(all(v));
+    sort(all(v));
+    reverse(all(v));
+    loop(i,0,m){
+        ll j = 0;
+        // if(q[i]==0) continue;
+        while(j<n && q[i]!=0){
+            ll a, b;
+            if(q[i]-v[j][1]>=0){
+                a= q[i]-v[j][1]+1; b = v[j][1]- v[j][0];
+                ctr+= a/b + a%b;
+                q[i] = v[j][0];
+            }
+            j++;
+        }
+    }
+    cout<<2*ctr<<endl;
+}
+
+int32_t main() {
+    auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
+
+    fastio();
+    // #ifndef ONLINE_JUDGE
+    //     freopen("error.txt", "w", stderr);
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    int tt_ = 1;
+    // cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+    // auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
