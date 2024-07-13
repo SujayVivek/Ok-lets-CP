3923c3c88e533b3d2109fd93f8901781fe5d7551
SujayVivek <sujayv2016@gmail.com>
2024-07-13 13:40:36 +0530

    .

---
+++ A_Strange_Birthday_Party.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//   _______ _     _                     _        _        __             _   _            _                            
+//  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
+//     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
+//     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
+//     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
+//     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define MOD 1000000007
+typedef long long ll;
+#define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
+#define loopm(ii, i, n, p) for (ll ii = i; ii < n; ii+p)
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+typedef unsigned long long ull;
+typedef long double lld;
+typedef vector<int> vi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vi> vvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+
+int Rec(int ind, vi &dp, vi &hash, vi &k, vi &c){
+    int n = dp.size(); int m = hash.size();
+    if(ind<0){
+        return 0;
+    }
+    if(dp[ind]!=-1){
+        return dp[ind];
+    }
+    int l,r;
+    
+    if(hash[ind]!=1){
+        hash[ind] =1;
+        l = Rec(ind-1, dp, hash, k ,c)+ c[ind];
+    }
+    r = Rec(ind-1, dp, hash, k, c) + c[k[ind]-1];
+    
+    cout<< l<< " "<<r<<"HI"<<endl;
+    return dp[ind] = min(l,r);
+   
+}
+void Solve() {
+    // Your code for each test case goes here
+    int n,m;cin>>n>>m;
+    vi k(n,0), c(m,0);
+    for(auto &x:k){
+        cin>>x;
+    }
+    for(auto &x:c){
+        cin>>x;
+    }
+    vi dp(n,-1);
+    vi hash(m,-1);
+    int q = min(m,n);
+    int p = Rec(q-1, dp, hash, k, c);
+    
+    if(n>m){
+        for(int i = n-1; i>= m;i--){
+            p+= c[k[i]-1];
+        }
+    }
+    cout<< p <<endl;
+    return;
+}
+
+int32_t main() {
+    auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
+
+    fastio();
+    // #ifndef ONLINE_JUDGE
+    //     freopen("error.txt", "w", stderr);
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+    // auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
