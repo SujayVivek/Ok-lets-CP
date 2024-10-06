549439caccbf26b711e9b1eac3d83e1d3b2cc7ed
SujayVivek <sujayv2016@gmail.com>
2024-10-06 15:31:20 +0530

    HI

---
+++ C_1_Adjust_The_Presentation_Easy_Version.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//   _______ _     _                     _        _        __             _   _            _                            
+//  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
+//     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
+//     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
+//     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
+//     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define MOD 1000000007
+typedef long long ll;
+#define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
+#define loopm(ii, i, n, p) for (ll ii = i; ii < n; ii+p)
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+typedef unsigned long long ull;
+typedef long double lld;
+typedef vector<int> vi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vi> vvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+
+void Solve() {
+    // Your code for each test case goes here
+    int n, m, q;cin>>n>>m>>q;
+    vi a(n,0);map<int, int> mpp;
+    loop(i,0,n){
+        cin>>a[i];
+        mpp[a[i]] = i;
+    }
+    vi b(m,0);
+    loop(i,0,m){
+        cin>>b[i];
+    }
+    int mxPos = 0;
+    map<int,int>Used;
+    int j = 0;
+    loop(i,0,m){
+        if(b[i] == a[j]){
+           Used[b[i]]++; 
+           j++;
+        }
+        else{
+            if(Used[b[i]]==0){
+                cout<<"TIDAK"<<endl;return;
+            }
+        }
+        if(j>n-1){
+            break;
+        }
+    }
+    cout<<"YA"<<endl;return;
+
+}
+
+int32_t main() {
+    auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
+
+    fastio();
+    // #ifndef ONLINE_JUDGE
+    //     freopen("error.txt", "w", stderr);
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+    // auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
+
+// if(mpp[b[i]]<=mxPos+1 && Used[mxPos]!=0){
+//             mxPos = max(mxPos, mpp[b[i]]);
+//         }else if(mpp[b[i]]==mxPos){
+//             Used[b[i]]++;
+//         }
+//         else{
+//             cout<<"TIDAK"<<endl;return;
+//         }
