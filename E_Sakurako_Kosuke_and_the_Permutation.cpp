7c51935c3cc8f843584699c08be1e9c641d911f1
SujayVivek <sujayv2016@gmail.com>
2024-10-25 20:32:02 +0530

    dfs

--- E_Sakurako_Kosuke_and_the_Permutation.cpp
+++ E_Sakurako_Kosuke_and_the_Permutation.cpp
@@ -1,50 +1,94 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+//   _______ _     _                     _        _        __             _   _            _                            
+//  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
+//     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
+//     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
+//     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
+//     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define MOD 1000000007
 typedef long long ll;
 #define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
+#define loopm(ii, i, n, p) for (ll ii = i; ii < n; ii+p)
+#define INF 1e18
 #define endl "\n"
 #define pb push_back
+#define ppb pop_back
 #define MP make_pair
 #define ff first
 #define ss second
+#define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
+typedef unsigned long long ull;
+typedef long double lld;
 typedef vector<int> vi;
 typedef vector<pair<ll, ll>> vpii;
 typedef vector<vi> vvi;
 typedef vector<long long> vll;
 typedef vector<vll> vvll;
 
-     map<int,int> mpp;
-
-void swap(vll &p, ll i, ll j){
-    mpp[p[i]] = j;
-    mpp[p[j]] = i;
-     ll temp = p[i];
-     p[i] = p[j];
-     p[j] = temp;
+vvll g;
+vll vis;
+ll c = 0;
+void dfs(ll nn){
+    vis[nn]= 1;
+    c++;
+    for(auto &v: g[nn]){
+        if(!vis[v]){
+            dfs(v);
+        }
+    }
 }
 void Solve() {
-     ll n;cin>>n;
-     vll p(n+1,0);
-     for(int i = 1; i<=n ; i++){
+    // Your code for each test case goes here
+    ll n;cin>>n;
+    vi p(n+1,0);
+    g.assign(n+1,{});
+    vis.assign(n+1, 0);
+    for(int i = 1; i<=n; i++){
         cin>>p[i];
-        mpp[p[i]] = i;
-     }
-    ll c = 0;
-    for(int i = 1 ; i<=n/2+1; i++){
-        if(p[i]==i || p[p[i]]==i)continue;
-        swap(p,p[i],mpp[i]);c++;
+        g[i].pb(p[i]);
     }
-    cout<<c<<endl;
-}
+    vll ans;
+    for(int i = 1; i<=n; i++){
+        c = 0;
+        if(!vis[i]){
+            dfs(i);
+        }
+        ans.pb(c);
+    }
+    ll res = 0;
+    for(auto x: ans){
+        if(x%2==0) res+= max(x/2 - 1,0ll);
+        else res+= x/2;
+    }
+    cout<<res<<endl;return;
+}   
 
 int32_t main() {
+    auto begin = chrono::high_resolution_clock::now(); // Initialize the begin time measurement
+
+    fastio();
+    // #ifndef ONLINE_JUDGE
+    //     freopen("error.txt", "w", stderr);
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
     int tt_ = 1;
     cin >> tt_;
     while (tt_--) {
         Solve();
     }
+
+    // auto end = chrono::high_resolution_clock::now();
+    // auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
     return 0;
 }
