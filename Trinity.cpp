48b7e228365afeae578d09c92ca4cb59c5056ce2
SujayVivek <sujayv2016@gmail.com>
2024-11-02 11:05:13 +0530

    contest

---
+++ Trinity.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define MOD 1000000007
+typedef long long ll;
+#define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
+#define endl "\n"
+#define pb push_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+typedef vector<int> vi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vi> vvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+#define int long long
+void Solve() {
+        int n;
+		cin>>n;
+        vi a(n+1,0);
+        int ans = 1e9;
+		for(int i=1;i<=n;i++) cin>>a[i];
+		sort(all(a));
+		for(int i=1;i<n;i++){
+			int x=lower_bound(all(a),a[i]+a[i+1])-a.begin();
+            ans=min(ans,n-x+1+i-1);
+		}
+		cout<<ans<<endl;
+}
+
+int32_t main() {
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+    return 0;
+}
