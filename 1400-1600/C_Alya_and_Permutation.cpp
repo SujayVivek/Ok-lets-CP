ee67a695986aca10570853c99880b296d277aced
SujayVivek <sujayv2016@gmail.com>
2024-11-30 19:41:10 +0530

    1700

--- 1400-1600/C_Alya_and_Permutation.cpp
+++ 1400-1600/C_Alya_and_Permutation.cpp
@@ -1,59 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define MOD 1000000007
-typedef long long ll;
-#define loop(ii, i, n) for (ll ii = i; ii < n; ++ii)
-#define endl "\n"
-#define pb push_back
-#define MP make_pair
+#define mod 1000000007
 #define ff first
 #define ss second
-#define all(x) (x).begin(), (x).end()
-typedef vector<int> vi;
-typedef vector<pair<ll, ll>> vpii;
-typedef vector<vi> vvi;
-typedef vector<long long> vll;
-typedef vector<vll> vvll;
+typedef vector<vector<long long>> vvi;
+typedef vector<long long> vi;
+#define int long long
+#define endl "\n"
 
 void Solve() {
-    int n;cin>>n;
-    int k = 0;
+    int n; cin>>n;
+    int p = 64-__builtin_clz(n);
     vi ans;
-    if(n%2==1){
-        ans.pb(2); ans.pb(1);
-        k&=2;
-        k|=1;
-        for(int i = 3; i<=n; i++){
-            if(i%2==1)k&=i;
-            else k|=i;
-            ans.pb(i);
+    if(n%2==0){
+        int r = (1<<(p-1))-1;
+        if(n-r>=3){
+                for(int i = 1; i<=n-1; i++){
+                if(i==r)continue;
+                ans.push_back(i);
+            }
+            ans.push_back(n);
+            ans.push_back(r);
+        }else{
+            n--;
+            for(int i = 2; i<=n-2; i++){
+                ans.push_back(i);
+            }
+            ans.push_back(1);
+            ans.push_back(n-1);
+            ans.push_back(n);
+            ans.push_back(n+1);
+            n++;
         }
+        
+    }else{
+        for(int i = 2; i<=n-2; i++){
+            ans.push_back(i);
+        }
+        ans.push_back(1);
+        ans.push_back(n-1);
+        ans.push_back(n);
     }
-    else{
-        int b = 32-__builtin_clz(n);
-        int p = pow(2, b-1)-1;
-        k&=2; k|=1;
-        ans.pb(2); ans.pb(1);
-        for(int i = 3; i<p; i++){
-            if(i%2==0)k|=i;
-            else k&=i;
-            ans.pb(i);
+    int k = 0;
+    for(int i = 0; i<n; i++){
+        if(i%2==0){
+            k&=ans[i];
+        }else{
+            k|=ans[i];
         }
-        for(int i = p; i<n; i++){
-            if(i%2==1) k&=(i+1);
-            else k|=(i+1);
-            ans.pb(i+1);
-        }
-        ans.pb(p);
-        k|=p;
-        // k = pow(2,b)-1;
     }
     cout<<k<<endl;
-    for(auto it: ans){
-        cout<<it<<" ";
-    }
-    cout<<endl;
+    for(auto &x: ans){
+        cout<<x<<" ";
+    }cout<<endl;
+    return;
 }
 
 int32_t main() {
