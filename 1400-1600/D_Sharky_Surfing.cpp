d17a9f9ddd4cc81c50ee2721132e2ddf8f8d6ab2
SujayVivek <sujayv2016@gmail.com>
2024-12-02 20:01:17 +0530

    lol

--- 1400-1600/D_Sharky_Surfing.cpp
+++ 1400-1600/D_Sharky_Surfing.cpp
@@ -10,21 +10,39 @@ typedef vector<long long> vi;
 
 void Solve() {
     int n, m, L;
-    cin>>n>>m>>L;
-    vector<pair<int,int>>hurdles;
-    for(int i = 0; i<n; i++){
-        int l, r; cin>>l>>r;
-        hurdles.push_back({l,r});
-    }
-    vector<pair<int,int>>powers;
-    int Sum = 0;
-    for(int i = 0; i<m; i++){
-        int x, y; cin>>x>>y;
-        powers.push_back({x,y});
-        Sum+=y;
+    cin >> n >> m >> L;
+    
+    vector<int> l(n), r(n);
+    for (int i = 0; i < n; i++) {
+        cin >> l[i] >> r[i];
     }
     
-}   
+    priority_queue<int> pq;
+    
+    vector<int> x(m), v(m);
+    for (int i = 0; i < m; i++) {
+        cin >> x[i] >> v[i];
+    }
+    
+    int cur = 1;
+    int ans = 0;
+    for (int i = 0, j = 0; i < n; i++) {
+        while (j < m && x[j] < l[i]) {
+            pq.push(v[j]);
+            j++;
+        }
+        while (cur < r[i] - l[i] + 2) {
+            if (pq.empty()) {
+                cout << -1 << endl;
+                return;
+            }
+            cur += pq.top();
+            pq.pop();
+            ans++;
+        }
+    }
+    cout << ans << "\n";
+}
 
 int32_t main() {
     int tt_ = 1;
