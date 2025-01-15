774cba6909a4a661c29ae10fff5f28b65f7f562e
SujayVivek <sujayv2016@gmail.com>
2025-01-16 00:13:22 +0530

    AWESOME

--- 800-1000/B_Nene_and_the_Card_Game.cpp
+++ 800-1000/B_Nene_and_the_Card_Game.cpp
@@ -1,34 +1,32 @@
-
-//   _______ _     _                     _        _        __             _   _            _                            
-//  |__   __| |   (_)                   | |      (_)      / _|           | | | |          (_)                           
-//     | |  | |__  _ ___    ___ ___   __| | ___   _ ___  | |_ ___  _ __  | |_| |__   ___   _ _ __  ___  __ _ _ __   ___ 
-//     | |  | '_ \| / __|  / __/ _ \ / _` |/ _ \ | / __| |  _/ _ \| '__| | __| '_ \ / _ \ | | '_ \/ __|/ _` | '_ \ / _ \
-//     | |  | | | | \__ \ | |_| (_) | |_| |  __/ | \__ \ | || (_) | |    | |_| | | |  __/ | | | | \__ \ |_| | | | |  __/
-//     |_|  |_| |_|_|___/  \___\___/ \__,_|\___| |_|___/ |_| \___/|_|     \__|_| |_|\___| |_|_| |_|___/\__,_|_| |_|\___|
-                                                                                                                     
 #include <bits/stdc++.h>
-using namespace std;                                                                                                                     
-#define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
-typedef vector<int> vi;
-typedef vector<long long> vll;
-#define PB push_back
-#define ll long long
+using namespace std;
+#define mod 1000000007
+#define ff first
+#define ss second
+typedef vector<vector<long long>> vvi;
+typedef vector<long long> vi;
+#define int long long
+#define endl "\n"
 
-int main(){
-    int t;cin>>t;
-    while(t--){
-        int n;cin>>n;
-        vi hash(n+1,0);
-        FOR(i,n){
-            int l;cin>>l;
-            hash[l]++;
-        }int ctr=0;
-        FOR(i,n+1){
-            if(hash[i]==2){
-                ctr++;
-            }
-        }
-        cout<<ctr<<endl;
+void Solve() {
+    int n; cin>>n;
+    map<int,int> mpp;
+    for(int i = 0; i<n; i++){
+        int x; cin>>x;
+        mpp[x]++;
+    }
+    int ans = 0;
+    for(auto it: mpp){
+        ans+= it.second/2;
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
     }
     return 0;
 }
