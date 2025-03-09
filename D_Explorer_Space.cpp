0ef6cd657c31879341713dcf8bad84d8412dd421
SujayVivek <sujayv2016@gmail.com>
2025-03-09 12:39:53 +0530

    DP

--- D_Explorer_Space.cpp
+++ D_Explorer_Space.cpp
@@ -7,62 +7,48 @@ typedef vector<vector<long long>> vvi;
 typedef vector<long long> vi;
 #define int long long
 #define endl "\n"
-typedef pair<int,int> pii;
 
-int dfs(int node, vector<vector<pii>> &g, int k, vi &vis){
+int n, m, k; 
+vvi h, v;
+int dp[505][505][23];
+
+int calc(int i, int j, int K){
+    if(i<0 || i>n-1 || j<0 || j>m-1) return 1e18;
+    if(K == 0) return 0;
+    if(dp[i][j][K]!=1e18) return dp[i][j][K];
     int ans = 1e18;
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-    pq.emplace(0, node, k/2);
-    while(!pq.empty()){
-        auto Top = pq.top(); pq.pop();
-        int cost = get<0>(Top);
-        int nod = get<1>(Top);
-        int remaining = get<2>(Top);
-        vis[node] = 1;
-        if(remaining==0){ans = min(ans,cost); continue;}
-        for(auto nn: g[nod]){
-            int nxt_nn = nn.first, dist = nn.second;
-            if(!vis[nxt_nn]) pq.emplace(dist + cost, nxt_nn, remaining-1);
-        }
-    }
-    return ans;
+    if(j<m) ans = min(ans, calc(i, j+1, K-2) + 2*h[i][j]);
+    if(j>0) ans = min(ans, calc(i, j-1, K-2) + 2*h[i][j-1]);
+    if(i<n) ans = min(ans, calc(i+1, j, K-2) + 2*v[i][j]);
+    if(i>0) ans = min(ans, calc(i-1, j, K-2) + 2*v[i-1][j]);
+    return dp[i][j][K] = ans;
 }
 void Solve() {
-    int n, m, k; cin>>n>>m>>k;
-    vector<vector<pii>> g(n*m+1, vector<pii>());
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<m; j++){
-            int a; cin>>a;
-            g[m*(i-1) + j].push_back({m*(i-1) + j+1, a});
-            g[m*(i-1) + j+1].push_back({m*(i-1)+ j, a});
+    cin>>n>>m>>k;
+    h.assign(n, vi (m));
+    v.assign(n, vi(m));
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m-1; j++){
+            cin>>h[i][j];
+        }
+    } 
+    for(int i = 0; i<n-1; i++){
+        for(int j = 0; j<m; j++){
+            cin>>v[i][j];
         }
     }
-    for(int i = 1; i<n; i++){
-        for(int j = 1; j<=m; j++){
-            int a; cin>>a;
-            g[m*(i-1)+j].push_back({m*(i) + j, a});
-            g[m*(i) + j].push_back({m*(i-1) + j, a});
-        }
-    }
-    if(k%2==1){
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                cout<<-1<<" ";
-            }cout<<endl;
-        }cout<<endl;return;
-    }
-    vvi grid(n+1, vi(m+1, 0));
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=m; j++){
-            vi vis(n*m + 1, 0);
-            grid[i][j] = dfs((i-1)*m+j, g, k, vis);
+    for(int i = 0; i<505; i++){
+        for(int j = 0; j<505; j++){
+            for(int k = 0; k<23; k++) dp[i][j][k] = 1e18;
         }
     }
     for(int i = 0; i<n; i++){
         for(int j = 0; j<m; j++){
-            cout<<2*grid[i+1][j+1]<<" ";
+            if(k%2==1) cout<<-1<<" ";
+            else
+            cout<< calc(i, j, k)<<" ";
         }cout<<endl;
-    }return;
+    }
 }
 
 int32_t main() {
